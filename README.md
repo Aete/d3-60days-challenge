496c23fda1af2b0192be2face6e6940963fd206f
Seunggyun Han <hsg8234@naver.com>
2021-02-19 09:58:07 +0900

    fix: change file names (week -> day)

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Week One: Bar Chart
+## Day One: Bar Chart
 
 - Bar chart with COVID-19 data in South Korea
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import WeekOne from './Week1/Week1';
+import DayOne from './Day1/DayOne';
 
 function App() {
   return (
     <div className="App">
-      <WeekOne />
+      <DayOne />
     </div>
   );
 }
